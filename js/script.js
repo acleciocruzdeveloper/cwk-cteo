7ca84c4dc0b963e1b97ad63c07fc3f9fcd10cc4d
Aclecio Cruz <aclecioscruz@gmail.com>
2021-06-24 17:47:57 -0300

    Organização do Code

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 function Enviar() {
 
-    var your_mail = document.getElementById("your_mail");
+    var your_mail = document.getElementById("your_mail")
 
     if (your_mail.value != "") {
-        alert('Dados enviados com sucesso!');
-    }
+        alert('Dados enviados com sucesso!')       
+    }else{
+        alert("Insira os dados corretamente")        
+    }     
 
 }
-
